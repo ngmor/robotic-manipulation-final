3d8fd5c8aae4385fd5dc07ac8d74b11d0cad1b58
Nick Morales <ngmorales97@gmail.com>
2022-11-26 17:26:42 -0600

    Refactored simulator code to match vector order of control code.

--- final/simulator.py
+++ final/simulator.py
@@ -12,23 +12,25 @@ def NextState(positions, velocities, velocity_limits,dt,accelerations=None):
         positions (np-array, 12-vector): current position of robot, in this order:
             [chassis phi, chassis x, chassis y, J1, J2, J3, J4, J5, W1, W2, W3, W4]
         velocities (np-array, 9-vector): control velocities, in this order:
-            [J1d, J2d, J3d, J4d, J5d, W1d, W2d, W3d, W4d]
+            [W1d, W2d, W3d, W4d, J1d, J2d, J3d, J4d, J5d]
         max_velocities (np-array, 9-vector): velocity limits, in this order:
-            [J1_lim, J2_lim, J3_lim, J4_lim, J5_lim, W1_lim, W2_lim, W3_lim, W4_lim]
+            [W1_lim, W2_lim, W3_lim, W4_lim, J1_lim, J2_lim, J3_lim, J4_lim, J5_lim]
         dt (float): timestep for integration
         accelerations (np-array, 9-vector, optional): accelerations. Defaults to None (0 for all).
             in this order:
-            [J1dd, J2dd, J3dd, J4dd, J5dd, W1dd, W2dd, W3dd, W4dd]
+            [W1dd, W2dd, W3dd, W4dd, J1dd, J2dd, J3dd, J4dd, J5dd]
     Returns:
         next_positions (np-array, 12-vector): next position of robot, in this order:
             [chassis phi, chassis x, chassis y, J1, J2, J3, J4, J5, W1, W2, W3, W4]
         next_joint_and_wheel_vel (np-array, 9-vector): next velocities, in this order:
-            [J1d, J2d, J3d, J4d, J5d, W1d, W2d, W3d, W4d]
+            [W1d, W2d, W3d, W4d, J1d, J2d, J3d, J4d, J5d]
     """    
 
     # Separate positions
     chassis_pos = np.array(positions[0:3])
-    joint_and_wheel_pos = np.array(positions[3:12])
+    joint_pos = np.array(positions[3:8])
+    wheel_pos = np.array(positions[8:12])
+    joint_and_wheel_pos = np.hstack((wheel_pos,joint_pos))
 
     # Make sure velocity limits are magnitudes
     velocity_limits_mag = np.abs(velocity_limits)
@@ -53,7 +55,7 @@ def NextState(positions, velocities, velocity_limits,dt,accelerations=None):
 
     # ODOMETRY
     # Get body twist and extract components
-    wheel_vel = joint_and_wheel_vel[5:9]
+    wheel_vel = joint_and_wheel_vel[0:4]
     body_twist = np.linalg.pinv(CHASSIS_H0) @ wheel_vel
     omega_bz = body_twist[0]
     v_bx = body_twist[1]
@@ -87,16 +89,14 @@ def NextState(positions, velocities, velocity_limits,dt,accelerations=None):
     # calculate new chassis position
     next_chassis_pos = chassis_pos + delta_q_s
 
-    next_positions = np.hstack((next_chassis_pos,next_joint_and_wheel_pos))
+    next_wheel_pos = next_joint_and_wheel_pos[0:4]
+    next_joint_pos = next_joint_and_wheel_pos[4:9]
+
+
+    next_positions = np.hstack((next_chassis_pos, next_joint_pos, next_wheel_pos))
 
     return next_positions, next_joint_and_wheel_vel
 
-"""
-A 12-vector representing the current configuration of the robot (3 variables for the chassis configuration, 5 variables for the arm configuration, and 4 variables for the wheel angles).
-A 9-vector of controls indicating the wheel speeds u (4 variables) and the arm joint speeds \dot{\theta} (5 variables).
-A timestep Δt.
-A positive real value indicating the maximum angular speed of the arm joints and the wheels. For example, if this value is 12.3, the angular speed of the wheels and arm joints is limited to the range [-12.3 radians/s, 12.3 radians/s]. Any speed in the 9-vector of controls that is outside this range will be set to the nearest boundary of the range. If you don't want speed limits, just use a very large number. If you prefer, your function can accept separate speed limits for the wheels and arm joints.
-"""
 
 if __name__ == "__main__":
 
@@ -110,7 +110,7 @@ if __name__ == "__main__":
 
     position_list = np.zeros((N,13))
     next_positions = np.copy(positions)
-    velocities[5:9] = [10,10,10,10]
+    velocities[0:4] = [10,10,10,10]
     next_velocities = np.copy(velocities)
     for i in range(N):
         position_list[i,0:12] = next_positions
@@ -121,7 +121,7 @@ if __name__ == "__main__":
 
     position_list = np.zeros((N,13))
     next_positions = np.copy(positions)
-    velocities[5:9] = [-10,10,-10,10]
+    velocities[0:4] = [-10,10,-10,10]
     next_velocities = np.copy(velocities)
     for i in range(N):
         position_list[i,0:12] = next_positions
@@ -132,7 +132,7 @@ if __name__ == "__main__":
 
     position_list = np.zeros((N,13))
     next_positions = np.copy(positions)
-    velocities[5:9] = [-10,10,10,-10]
+    velocities[0:4] = [-10,10,10,-10]
     next_velocities = np.copy(velocities)
     for i in range(N):
         position_list[i,0:12] = next_positions
@@ -142,8 +142,8 @@ if __name__ == "__main__":
 
     position_list = np.zeros((N,13))
     next_positions = np.copy(positions)
-    velocities[5:9] = [-10,10,-10,10]
-    max_velocities[5:9] = [5,5,5,5]
+    velocities[0:4] = [-10,10,-10,10]
+    max_velocities[0:4] = [5,5,5,5]
     next_velocities = np.copy(velocities)
     for i in range(N):
         position_list[i,0:12] = next_positions
@@ -153,7 +153,7 @@ if __name__ == "__main__":
 
     position_list = np.zeros((N,13))
     next_positions = np.copy(positions)
-    velocities[0:9] = [1,1,1,1,1,0,0,0,0]
+    velocities[0:9] = [0,0,0,0,1,1,1,1,1]
     next_velocities = np.copy(velocities)
     for i in range(N):
         position_list[i,0:12] = next_positions
